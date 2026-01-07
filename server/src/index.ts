1bed3c8198aeb739daa2ac925b613743ab19a8af
Spidy394 <79023855+Spidy394@users.noreply.github.com.>
2026-01-07 20:38:52 +0530

    express setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+import app from './app.ts';
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
